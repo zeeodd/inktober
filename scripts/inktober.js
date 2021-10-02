a6607e3404bde3738229bed8fbd4ddd4832811fc
zeeodd <evergreenskier@gmail.com>
2021-10-01 23:00:01 -0400

    scalability

---
+++ scripts/inktober.js
@@ -0,0 +1,31 @@
+var modal = document.getElementById('myModal');
+var modalImg = document.getElementById("img01");
+
+modal.onclick = function() {
+    img01.className += " out";
+    setTimeout(function() {
+       modal.style.display = "none";
+       img01.className = "modal-content";
+     }, 300);
+
+ }
+
+/*
+*   OCTOBER 1
+*/
+var toggle = document.getElementById('modalToggle1');
+var img = document.getElementById('myImg1');
+toggle.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+/*
+*   OCTOBER 2
+*/
+// var toggle = document.getElementById('modalToggle2');
+// var img = document.getElementById('myImg2');
+// toggle.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = img.src;
+// }
